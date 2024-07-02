8198bd68cc8e483bd31e39cf3b875dcc9b68f825
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-07-02 15:25:51 +0530

    imdb_movie_app initial commit

--- imdb_movie_app/src/App.jsx
+++ imdb_movie_app/src/App.jsx
@@ -6,7 +6,13 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1 className="text-red-500">IMDB MOvie App</h1>
+      <h5>He</h5>
+      <p>Hello</p>
+    </>
+  );
 }
 
 export default App;
